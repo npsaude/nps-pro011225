998a143e6d486a3b365e3f3b0eaa7225c5aee2dc
[dyad] <adriano.dapv@gmail.com>
2025-12-01 16:59:06 -0300

    [dyad] Corrigindo CORS na função Edge para evitar erro ao enviar documentos de descrição cirúrgica - wrote 1 file(s)

--- supabase/functions/process-descricao-cirurgica.ts
+++ supabase/functions/process-descricao-cirurgica.ts
@@ -8,6 +8,7 @@ const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
 interface UploadedFile {
